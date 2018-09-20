2186674f472dd2cd8935ecbbb9ab0dcad4b8913e
Frederic Torres <ftorres@emoneyadvisor.com>
2018-09-20 16:31:42 -0400

    Creartion

---
+++ PeakFindindUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeakFindindUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PeakFindindUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("45b18097-b70f-4103-b644-47304122ed8f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PeakFindindUnitTests/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// MIT Class https://www.youtube.com/watch?v=HtSuA80QTyo
+// https://www.filipekberg.se/2014/02/10/understanding-peak-finding/
+
+// C:\DVT\.NET\PeakFindindUnitTests\PeakFindindUnitTests\bin\Debug
+/*
+ set MSTEST=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\mstest
+ "%MSTEST%" /testcontainer:PeakFindindUnitTests.dll
+ */
+namespace PeakFindindUnitTests
+{
+
+    public class PeakFinder {
+        public static bool IsPeakElement(List<int> list, int index)
+        {
+            if(index == 0) // No left boundary
+                return (list[index] >= list[index+1]);
+            else if(index == list.Count-1) // No right boundary
+                return (list[index] >= list[index-1]);
+            else
+                return (list[index] >= list[index-1]) && (list[index] >= list[index+1]);
+        }
+        public static int FindPeakElement(List<int> list, int start, int last)
+        {
+            var index = ((last-start) / 2) + start; // Start in the middle
+
+            if(IsPeakElement(list, index))
+                return index;
+
+            // if we found a greater number on right, we expect to find one Peak element on the right side
+            if(list[index] < list[index+1])
+                return FindPeakElement(list, index+1, last);
+            // if we found a greater number on the left, we expect to find one Peak element on the left side
+            else if(list[index] < list[index-1])
+                return FindPeakElement(list, 0, index-1);
+
+            throw new ArgumentException();
+        }
+    }
+
+    [TestClass]
+    public class UnitTePeakFinderUnitTests
+    {
+        List<int> TestList0 = new List<int>() { 1, 3, 4, 3, 5, 1, 3 };
+
+         [TestMethod]
+        public void FindPeakElement_IsPeakElementAtIndex()
+        {
+            var l = new List<int> { 1, 3, 2 }; // Peak is 3[1]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+        }
+
+         [TestMethod]
+        public void FindPeakElement_OnTheRight()
+        {
+            var l = new List<int> { 1, 3, 2 }; // Peak is 3[1]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 2, 3, 4, 1 };// Peak is 4[3]
+            Assert.AreEqual(3, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 2, 3, 4, 5, 1 };// Peak is 5[4]
+            Assert.AreEqual(4, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 2, 3, 4, 5, 6, 1 };// Peak is 6[5]
+            Assert.AreEqual(5, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 2, 3, 4, 5, 6, 7 };// Peak is 7[6]
+            Assert.AreEqual(6, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+        }
+
+        [TestMethod]
+        public void IsPeakElement_WithinBoundary()
+        {
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 1, 3, 2 }, 1));
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 1, 1, 3, 2, 2 }, 2));
+
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 3, 3, 3 }, 1));
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 1, 3, 3, 3, 2 }, 2));
+
+            Assert.IsFalse(PeakFinder.IsPeakElement(new List<int> { 1, 0, 2 }, 1));
+            Assert.IsFalse(PeakFinder.IsPeakElement(new List<int> { 1, 1, 0, 2, 2 }, 2));
+        }
+        [TestMethod]
+        public void IsPeakElement_AtBoundaryLimitOnLeft()
+        {
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 3, 2 }, 0));
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 3, 2, 1, 0 }, 0));
+
+            Assert.IsFalse(PeakFinder.IsPeakElement(new List<int> { 1, 2 }, 0));
+            Assert.IsFalse(PeakFinder.IsPeakElement(new List<int> { 1, 2, 1, 0 }, 0));
+        }
+        [TestMethod]
+        public void IsPeakElement_AtBoundaryLimitOnRight()
+        {
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 3, 2, 3}, 2));
+            Assert.IsTrue(PeakFinder.IsPeakElement(new List<int> { 2, 3 }, 1));
+
+            Assert.IsFalse(PeakFinder.IsPeakElement(new List<int> { 3, 2, 1}, 2));
+            Assert.IsFalse(PeakFinder.IsPeakElement(new List<int> { 2, 1 }, 1));
+        }      
+    }
+}
