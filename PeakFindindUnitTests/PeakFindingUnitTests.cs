27dd8f5894b1f2e8c05d2ab05911bc536f4b3580
Frederic Torres <ftorres@emoneyadvisor.com>
2018-09-20 16:42:57 -0400

    Update

--- PeakFindindUnitTests/PeakFindingUnitTests.cs
+++ PeakFindindUnitTests/PeakFindingUnitTests.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// MIT Class https://www.youtube.com/watch?v=HtSuA80QTyo
-// https://www.filipekberg.se/2014/02/10/understanding-peak-finding/
-
-// C:\DVT\.NET\PeakFindindUnitTests\PeakFindindUnitTests\bin\Debug
-/*
- set MSTEST=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\mstest
- "%MSTEST%" /testcontainer:PeakFindindUnitTests.dll
- */
 namespace PeakFindindUnitTests
 {
 
@@ -52,15 +44,43 @@ namespace PeakFindindUnitTests
         {
             var l = new List<int> { 1, 3, 2 }; // Peak is 3[1]
             Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 3, 2 }; // Peak is 3[0]
+            Assert.AreEqual(0, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 2, 3 }; // Peak is 3[0]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 1, l.Count-1));
+        }
+
+        [TestMethod]
+        public void FindPeakElement_OnTheLeft()
+        {
+            var l = new List<int> { 1, 4, 3, 2, 1 };// Peak is 4[1]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 5, 4, 3, 2, 1 };// Peak is 5[1]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 5, 4, 3, 2, 1 };// Peak is 5[0]
+            Assert.AreEqual(0, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 6, 5, 4, 3, 2, 1 };// Peak is 6[1]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 6, 5, 4, 3, 2, 1 };// Peak is 6[0]
+            Assert.AreEqual(0, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 1, 7, 6, 5, 4, 3, 2, 1 };// Peak is 7[1]
+            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> { 7, 6, 5, 4, 3, 2, 1 };// Peak is 7[0]
+            Assert.AreEqual(0, PeakFinder.FindPeakElement(l, 0, l.Count-1));
         }
 
          [TestMethod]
         public void FindPeakElement_OnTheRight()
         {
-            var l = new List<int> { 1, 3, 2 }; // Peak is 3[1]
-            Assert.AreEqual(1, PeakFinder.FindPeakElement(l, 0, l.Count-1));
-
-            l = new List<int> { 1, 2, 3, 4, 1 };// Peak is 4[3]
+            var l = new List<int> { 1, 2, 3, 4, 1 };// Peak is 4[3]
             Assert.AreEqual(3, PeakFinder.FindPeakElement(l, 0, l.Count-1));
 
             l = new List<int> { 1, 2, 3, 4, 5, 1 };// Peak is 5[4]
@@ -71,6 +91,9 @@ namespace PeakFindindUnitTests
 
             l = new List<int> { 1, 2, 3, 4, 5, 6, 7 };// Peak is 7[6]
             Assert.AreEqual(6, PeakFinder.FindPeakElement(l, 0, l.Count-1));
+
+            l = new List<int> {1, 2, 6, 5, 3, 7, 4};// Peak is 2[6]
+            Assert.AreEqual(2, PeakFinder.FindPeakElement(l, 0, l.Count-1));
         }
 
         [TestMethod]
